091eb258fd07550d43ac18c3ab5f53d31c833c6e
Choi33 <choi333ys@gmail.com>
2020-12-17 13:37:07 +0900

    백준-2468

--- src/BackJoon2468.java
+++ src/BackJoon2468.java
@@ -4,12 +4,13 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.security.cert.TrustAnchor;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.StringTokenizer;
 import java.util.TreeMap;
 
 public class BackJoon2468 {
 
-    static int[][] zone;
+    static int[][] zone, working;
     static int size, max, min;
     static Boolean[][] visited;
     static ArrayList<Integer> numList;
@@ -17,7 +18,7 @@ public class BackJoon2468 {
     public static Boolean ZoneNum(int x, int y){
         if (x<=-1 || x>=size || y<=-1|| y>=size)
             return false;
-        if (visited[x][y]==false){
+        if (zone[x][y]!=0 && visited[x][y]==false){
             visited[x][y]=true;
             ZoneNum(x-1,y);
             ZoneNum(x+1,y);
@@ -41,16 +42,21 @@ public class BackJoon2468 {
             tokenizer=new StringTokenizer(reader.readLine());
             for(int j=0; j<size; j++){
                 zone[i][j]=Integer.parseInt(tokenizer.nextToken());
+
             }
         }
 
-        min=zone[0][0];
+        working=new int[size][size];
+        for(int t=0; t<size;t++){
+            for(int y=0; y<size; y++){
+                working[t][y]=zone[t][y];
+            }
+        }
+
         max=zone[0][0];
 
         for (int a=0; a<size; a++){
             for (int b=1; b<size; b++){
-                if(min>zone[a][b])
-                    min=zone[a][b];
                 if(max<zone[a][b])
                     max=zone[a][b];
             }
@@ -61,18 +67,22 @@ public class BackJoon2468 {
         numList=new ArrayList<>();
 
         int point=max;
-        while (point>min){
 
+        while (point>=0){
 
             for(int z=0; z<size; z++){
                 for(int v=0; v<size; v++){
-                    zone[z][v]-=point;
+                    int temp=working[z][v]-point;
+                    if (temp<0)
+                        working[z][v]=0;
+                    else
+                        working[z][v]=temp;
                 }
             }
 
             for(int q=0; q<size; q++){
                 for(int p=0; p<size; p++){
-                    if(zone[q][p]<=0)
+                    if(working[q][p]==0)
                         visited[q][p]= true;
                     else
                         visited[q][p]=false;
@@ -84,20 +94,30 @@ public class BackJoon2468 {
             int result=0;
             for (int g=0; g<size; g++){
                 for(int h=0; h<size; h++){
-                    if (ZoneNum(g,h)== true){
-                        result++;
+                    if(zone[g][h]!=0){
+                        if(ZoneNum(g,h)==true){
+                            result++;
+                        }
                     }
+
                 }
             }
             numList.add(result);
+            for (int r=0; r<size; r++){
+                for(int e=0; e<size; e++){
+                    working[r][e]=zone[r][e];
+                }
+            }
 
             point--;
 
 
 
+
         }
+        Collections.sort(numList);
 
+        System.out.println(numList.get(numList.size()-1));
 
-        System.out.println(numList);
     }
 }
