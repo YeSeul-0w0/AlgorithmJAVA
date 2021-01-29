fc8245141555dec35618546031e5e50ecd139f62
Choi33 <choi333ys@gmail.com>
2021-01-29 19:43:20 +0900

    백업

---
+++ src/BackJoon13023.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BackJoon13023 {
+    static int people;
+    static int total;
+
+    static int[][] relationship;
+    static Boolean[][] visited;
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader=new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer tokenizer=new StringTokenizer(reader.readLine());
+
+        people=Integer.parseInt(tokenizer.nextToken());
+        total =Integer.parseInt(tokenizer.nextToken());
+
+        relationship =new int[people][people];
+
+
+        for(int i = 0; i< total; i++){
+            tokenizer=new StringTokenizer(reader.readLine());
+            int A=Integer.parseInt(tokenizer.nextToken());
+            int B=Integer.parseInt(tokenizer.nextToken());
+            relationship[A][B]=1;
+        }
+
+        visited=new Boolean[people][people];
+        for(int a=0; a<people; a++){
+            for(int b=0; b<people; b++){
+                visited[a][b]=false;
+            }
+        }
+
+        for (int p=0; p<people; p++){
+            for(int q=0; q<people; q++){
+                if (relationship[p][q]==1){
+
+                }
+            }
+        }
+
+    }
+}
---
+++ src/BackJoon2670.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BackJoon2670 {
+
+    static int N;
+    static double[] num_list,dp;
+    static double ans,mul;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader=new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer tokenizer=new StringTokenizer(reader.readLine());
+
+        N=Integer.parseInt(tokenizer.nextToken());
+
+        num_list= new double[N];
+        dp=new double[N+1];
+
+        for(int i=0; i<N; i++){
+            double A=Double.parseDouble(reader.readLine());
+            num_list[i]=A;
+        }
+
+        ans=0;
+        mul=0;
+
+        for(int a=0; a<N; a++){
+            mul=num_list[a];
+            dp[a]=Math.max(dp[a],mul);
+            for (int b=a+1;b<N;b++){
+                mul *= num_list[b];
+                dp[a]=Math.max(ans,dp[a]);
+            }
+        }
+
+
+        System.out.println(String.format("%.3f", ans));
+    }
+}
