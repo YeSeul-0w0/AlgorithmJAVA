7323dca442fb42515a5f717436e892b08022cc6f
Choi33 <choi333ys@gmail.com>
2021-02-19 19:27:04 +0900

    백준 2156

---
+++ src/BackJoon2156.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class BackJoon2156 {
+
+    public static void main(String[] args){
+
+        Scanner scanner=new Scanner(System.in);
+
+        int N=scanner.nextInt();
+
+        int[] number=new int[N+2];
+        for(int i=1; i<=N; i++){
+            number[i]=scanner.nextInt();
+        }
+
+        int[] dp=new int[10002];
+        dp[1]=number[1];
+        dp[2]=Math.max(number[1]+number[2], number[2]);
+
+        for(int i=3;i<=N;i++) {
+            dp[i] = Math.max(dp[i-2] + number[i], dp[i-3] + number[i-1]+number[i]);
+            dp[i]=Math.max(dp[i-1],dp[i]);
+        }
+
+        System.out.println(dp[N]);
+    }
+}
