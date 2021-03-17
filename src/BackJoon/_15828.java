5d0b8f64594de79accf00be89cfeeaf1c22b7b65
Choi33 <choi33ys@gmail.com>
2021-03-17 13:40:31 +0900

    백준_15828(미완성)

---
+++ src/BackJoon/_15828.java
@@ -0,0 +1,44 @@
+package BackJoon;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class _15828 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+
+        int que_size= Integer.parseInt(br.readLine());
+        int check=0;
+        Queue<Integer> queue=new LinkedList<>();
+
+        while(check!=-1){
+            check=Integer.parseInt(br.readLine());
+            if(check==0){
+                queue.poll();
+            }
+            else if(check>0){
+                if(queue.size()<que_size) {
+                    queue.offer(check);
+                }
+            }
+            else{
+                break;
+            }
+        }
+        if(queue.isEmpty()){
+            System.out.println("empty");
+
+        }
+        else{
+            while (!queue.isEmpty()){
+                System.out.println(queue.poll());
+            }
+        }
+
+    }
+}
