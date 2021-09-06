8b7b7d48418f11927b230cb80d0fc747aff35ac4
Choi33 <choi333ys@gmail.com>
2021-09-06 13:19:45 +0900

    백업

--- src/Programmers/_210901.java
+++ src/Programmers/_210901.java
@@ -2,60 +2,58 @@ package Programmers;
 
 public class _210901 {
     public static int[][] connection;
-    public static boolean [] visited;
+    public static int[][] dp;
     public static int answer;
-
-    public int checking_load(int position){
-        boolean flag=false;
-
-        for (int i = 1; i < connection.length; i++) {
-            if(visited[i]==false && connection[position][i]==1){
-                visited[i]=true;
-                answer++;
-                flag=true;
-                break;
-            }
-        }
-        if (!flag){
-            return -1;
-        }
-        else{
-            return answer;
-        }
-    }
 
     public int solution(int n, int m, int[][] edge_list, int k, int[] gps_log) {
         answer = 0;
-        
-        connection=new int[n+1][n+1];
+
+        connection = new int[n + 1][n + 1];
         for (int a = 0; a < m; a++) {
-            connection[edge_list[a][0]][edge_list[a][1]]=1;
-            connection[edge_list[a][1]][edge_list[a][0]]=1;
+            connection[edge_list[a][0]][edge_list[a][1]] = 1;
+            connection[edge_list[a][1]][edge_list[a][0]] = 1;
         }
 
-        visited=new boolean[n+1];
+        dp = new int[k + 1][n + 1];
 
-        for (int i = 0; i < k-1; i++) {
-            if(gps_log[i]!=gps_log[i+1] && connection[gps_log[i]][gps_log[i+1]]==1){
-                visited[gps_log[i]]=true;
-                continue;
+
+        int INF = 201;
+
+        for (int b = 0; b < k + 1; b++) {
+            for (int c = 0; c < n + 1; c++) {
+                dp[b][c] = INF;
             }
-            else if(gps_log[i]!=gps_log[i+1] && connection[gps_log[i]][gps_log[i+1]]==0){
+        }
 
-                if(checking_load(gps_log[i+1])==-1){
-                    answer=-1;
-                    break;
+        dp[0][gps_log[0]] = 0;
+
+
+        for (int i = 1; i < k; i++) {
+            for (int j = 1; j < n + 1; j++) {
+                dp[i][j] = Math.min(dp[i][j], dp[i - 1][j]);
+                for (int z = 1; z < n + 1; z++) {
+                    if (connection[j][z] == 1) {
+                        dp[i][j] = Math.min(dp[i][j], dp[i - 1][z]);
+                    }
+                }
+                if (j != gps_log[i]) {
+                    dp[i][j]++;
                 }
             }
         }
-
-        return answer;
+        if (dp[k - 1][gps_log[k - 1]] < INF) {
+            return dp[k - 1][gps_log[k - 1]];
+        } else {
+            return -1;
+        }
     }
+
 
     public static void main(String[] args) {
-        _210901 test=new _210901();
+        _210901 test = new _210901();
 
-        test.solution(7,10, new int[][] {{1, 2}, {1, 3}, {2, 3}, {2, 4}, {3, 4}, {3, 5}, {4, 6}, {5, 6}, {5, 7}, {6, 7}},
-                6,new int[]{1, 2, 3, 3, 6, 7});
+        test.solution(7, 10, new int[][]{{1, 2}, {1, 3}, {2, 3}, {2, 4}, {3, 4}, {3, 5}, {4, 6}, {5, 6}, {5, 7}, {6, 7}},
+                6, new int[]{1, 2, 3, 3, 6, 7});
     }
 }
+
