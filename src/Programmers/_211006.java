287403865d7c757e64e7061e5cd86772b036617e
Choi33 <choi333ys@gmail.com>
2021-10-03 20:49:37 +0900

    readme 수정

--- src/Programmers/_211006.java
+++ src/Programmers/_211006.java
@@ -17,7 +17,7 @@ public class _211006 {
             }
         }
 
-        for (int i = 1; i < len; i++) {
+        for (int i = 2; i < len; i++) {
             for (int j = 1; j < i+1; j++) {
                 if(s.charAt(i)==s.charAt(i-j) && dp[i-j+1][i-1]==1){
                     dp[i-j][i]=1;
